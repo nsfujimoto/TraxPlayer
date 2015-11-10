0d2a748ae43fc73f21887a3b28cadf38263f870b
nsfujimoto <fujimoto@ns.cs.ehime-u.ac.jp>
2015-11-10 15:27:44 +0900

    クリックでタイルをマスに配置できるまで

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace NSTraxPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureBox6);
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(784, 429);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(238, 333);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::NSTraxPlayer.Properties.Resources.TRW;
+            this.pictureBox6.Location = new System.Drawing.Point(133, 226);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::NSTraxPlayer.Properties.Resources.TLW;
+            this.pictureBox5.Location = new System.Drawing.Point(26, 226);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::NSTraxPlayer.Properties.Resources.BRW;
+            this.pictureBox4.Location = new System.Drawing.Point(133, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::NSTraxPlayer.Properties.Resources.BLW;
+            this.pictureBox3.Location = new System.Drawing.Point(26, 126);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::NSTraxPlayer.Properties.Resources.HW;
+            this.pictureBox2.Location = new System.Drawing.Point(133, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::NSTraxPlayer.Properties.Resources.VW;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(750, 750);
+            this.pictureBox7.TabIndex = 1;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(779, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1034, 774);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NSTraxPlayer
+{
+    public partial class Form1 : Form
+    {
+        int selected_tile = 0;
+        Bitmap canvas;
+        Graphics g;
+        Image[] img = new Image[33];
+        const int img_width = 30;
+        const int img_height = 30;
+        const int BMAX = 25;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initimg();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void initimg()
+        {
+            img[1] = new Bitmap(GetType(), "vertical_w.png");
+            img[2] = new Bitmap(GetType(), "horizontal_w.png");
+            img[4] = new Bitmap(GetType(), "upper_left_w.png");
+            img[8] = new Bitmap(GetType(), "lower_right_w.png");
+            img[16] = new Bitmap(GetType(), "upper_right_w.png");
+            img[32] = new Bitmap(GetType(), "lower_left_w.png");
+            canvas = new Bitmap(pictureBox7.Width, pictureBox7.Height);
+            g = Graphics.FromImage(canvas);
+            pictureBox7.Image = canvas;
+
+            for (int i = 0; i < BMAX; i++)
+            {
+                g.DrawLine(Pens.Gray, 0, i * img_height-1, 30 * BMAX, i * img_height-1);
+                g.DrawLine(Pens.Black, i * img_width-1, 0, i * img_width-1, 30 * BMAX);
+            }
+        }
+
+
+
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            Point formClientCurPos = pictureBox7.PointToClient(Cursor.Position);
+            formClientCurPos = new Point((int)formClientCurPos.X / img_width, (int)formClientCurPos.Y / img_height);
+            label1.Text = formClientCurPos.ToString();
+
+            if(selected_tile != 0)
+            {
+                g.DrawImage(img[selected_tile], img_width * formClientCurPos.X, img_height * formClientCurPos.Y);
+                pictureBox7.Refresh();
+                selected_tile = 0;
+                pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+
+
+        }
+
+
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            selected_tile = 1;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            selected_tile = 2;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            selected_tile = 8;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            selected_tile = 4;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            selected_tile = 32;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            selected_tile = 16;
+            pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+        }
+    }
+}
